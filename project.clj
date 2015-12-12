8efc051b40bd8a3c7a6c5fc37332389730209129
Matt Pladna <mpladna@lgscout.com>
2015-12-11 23:54:47 -0500

    adding lein bin

--- project.clj
+++ project.clj
@@ -7,5 +7,7 @@
                  [org.clojure/tools.cli "0.3.3"]
                  [cheshire "5.5.0"]]
   :main ^:skip-aot edn-to-json.core
+  :bin {:name "edn2json" :bin-path "bin"}
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:plugins [[lein-bin "0.3.5"]]}})
