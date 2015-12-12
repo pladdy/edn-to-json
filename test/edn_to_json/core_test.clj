9379c96267be388f5c03ef993163ed3c009f641e
Matt Pladna <pladdypants@gmail.com>
2015-12-12 16:07:11 -0500

    fixing and adding test

--- test/edn_to_json/core_test.clj
+++ test/edn_to_json/core_test.clj
@@ -3,6 +3,8 @@
             [clojure.java.io :as io]
             [edn-to-json.core :as ej]))
 
+(def not-nil? (complement nil?))
+
 (deftest is-edn-file-test
   (testing "a given file is edn"
     (let [test-edn-file "is-edn-file-test.edn"]
@@ -10,4 +12,9 @@
       (is (ej/is-edn-file? test-edn-file) "  file should be edn format")
       (io/delete-file test-edn-file))))
 
-
+(deftest usage-test
+  (testing "make sure usage returns content"
+    (let [test-summary "-a --a-thing A thing"]
+      (and
+        (is (not-nil? (ej/usage test-summary))) "  shouldn't be nil")
+        (is (nil? (re-find #"^$" (ej/usage test-summary))) "  shouldn't be empty string"))))
